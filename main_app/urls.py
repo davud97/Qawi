82b9cf23d4cce9aa8b8c383ee68c328e6d4df3bb
davud97 <dawoodilyas48@gmail.com>
2026-02-11 17:01:52 +0300

    urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,3 +1,16 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    # Landing page is login if not authenticated
+    path('', views.landing, name='landing'),
+
+    # Home page (after login)
+    path('home/', views.home, name='home'),
+
+    # Authentication
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='landing'), name='logout'),
+]
