47e7a6d92eca840f50bea38fe1dddd94401aaa55
davud97 <dawoodilyas48@gmail.com>
2026-02-11 17:02:06 +0300

    urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -13,4 +13,12 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='landing'), name='logout'),
+
+
+    # Classes & Workouts
+    path('classes/<int:class_id>/', views.class_detail, name='class_detail'),
+    path('workouts/<int:workout_id>/', views.workout_detail, name='workout_detail'),
+    path('classes/create/', views.create_class, name='create_class'),
+    path('classes/<int:class_id>/add_workout/', views.add_workout_plan, name='add_workout_plan'),
+    path('workouts/<int:workout_id>/add_exercise/', views.add_exercise, name='add_exercise'),
 ]
